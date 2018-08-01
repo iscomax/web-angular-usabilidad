a1b8beb60557c725c2f1784ce00b69fe3cdcc2bb
iscomax <santamariajc61@gmail.com>
2018-07-31 23:04:45 -0500

    prueba de usabilidad angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { Caja1Component } from './caja1/caja1.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    Caja1Component,
+    FooterComponent
   ],
   imports: [
     BrowserModule
